1bfbf5438500131825a475c08baad5025e7e5ab7
rodrilp <rodrigolp7@gmail.com>
2024-02-25 19:33:02 -0600

    Add DataLayer

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
+
+type Person struct {
+	Name string `json:"name,omitempty"`
+	Age  int    `json:"age,omitempty"`
+}
+
+// personID: Person
+var datalayer map[int]Person
+var personID int
+
+func init() {
+	datalayer = make(map[int]Person)
+	personID = 0
+
+	log.Println("Data Layer initiated")
+
+}
 
 func main() {
 	http.HandleFunc("/", healthHandler)
@@ -21,4 +39,40 @@ func main() {
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
 	w.Write([]byte("server is running..."))
+}
+
+// Datalayer
+func createPerson(p Person) int {
+	pID := personID + 1
+	datalayer[pID] = p
+
+	personID = pID
+
+	return pID
+}
+
+func readPerson(id int) (Person, error) {
+	if person, ok := datalayer[id]; ok {
+		return person, nil
+	}
+
+	return Person{}, fmt.Errorf("Person with id = %d not found", id)
+}
+
+func updatePerson(id int, p Person) error {
+	if _, ok := datalayer[id]; ok {
+		datalayer[id] = p
+		return nil
+	}
+
+	return fmt.Errorf("Person with id = %d not found", id)
+}
+
+func deletePerson(id int) error {
+	if _, ok := datalayer[id]; ok {
+		delete(datalayer, id)
+		return nil
+	}
+
+	return fmt.Errorf("Person with id = %d not found", id)
 }
