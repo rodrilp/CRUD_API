04aca99dfa417f864db2833c4aab79b7b5e52d07
rodrilp <rodrigolp7@gmail.com>
2024-02-27 20:44:36 -0600

    Add handlers for each CRUD method

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type Person struct {
 	Name string `json:"name,omitempty"`
 	Age  int    `json:"age,omitempty"`
+}
+
+type Response struct {
+	Message string `json:"message,omitempty"`
+	Data    Person `json:"data,omitempty"`
+	Error   string `json:"error,omitempty"`
 }
 
 // personID: Person
@@ -25,6 +33,7 @@ func init() {
 
 func main() {
 	http.HandleFunc("/", healthHandler)
+	http.HandleFunc("/person", personHandler)
 
 	log.Println("Server starting at port -> :4444")
 
@@ -39,6 +48,118 @@ func main() {
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
 	w.Write([]byte("server is running..."))
+}
+
+func personHandler(w http.ResponseWriter, r *http.Request) {
+	var res Response
+	// send the response at the end
+	defer func() {
+		json.NewEncoder(w).Encode(res)
+	}()
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(200)
+
+	switch r.Method {
+	case http.MethodPost:
+		var p Person
+
+		err := json.NewDecoder(r.Body).Decode(&p)
+		if err != nil {
+			res.Error = err.Error()
+			return
+		}
+
+		id := createPerson(p)
+		res.Message = fmt.Sprintf("New person created with id: %d", id)
+
+	case http.MethodGet:
+		strID := r.URL.Query().Get("id")
+
+		if strID == "" {
+			log.Println("Invalid ID")
+			res.Error = "Invalid ID"
+			w.WriteHeader(400)
+		}
+
+		// conver id into int
+		id, err := strconv.Atoi(strID)
+		if err != nil {
+			log.Println("Invalid id", err)
+			res.Error = err.Error()
+			w.WriteHeader(400)
+			return
+		}
+
+		p, err := readPerson(id)
+		if err != nil {
+			log.Println("Read erro", err)
+			res.Error = err.Error()
+			w.WriteHeader(400)
+		}
+
+		res.Data = p
+
+	case http.MethodPut:
+		strID := r.URL.Query().Get("id")
+
+		if strID == "" {
+			log.Println("Invalid ID")
+			res.Error = "Invalid ID"
+			w.WriteHeader(400)
+		}
+
+		// conver id into int
+		id, err := strconv.Atoi(strID)
+		if err != nil {
+			log.Println("Invalid id", err)
+			res.Error = err.Error()
+			w.WriteHeader(400)
+			return
+		}
+
+		// read json body and conver to struct
+		var p Person
+		json.NewDecoder(r.Body).Decode(&p)
+
+		err = updatePerson(id, p)
+		if err != nil {
+			log.Println("Update error", err)
+			res.Error = err.Error()
+			w.WriteHeader(400)
+			return
+		}
+
+		res.Message = "Person successfully updated."
+
+	case http.MethodDelete:
+		strID := r.URL.Query().Get("id")
+
+		if strID == "" {
+			log.Println("Invalid ID")
+			res.Error = "Invalid ID"
+			w.WriteHeader(400)
+		}
+
+		// conver id into int
+		id, err := strconv.Atoi(strID)
+		if err != nil {
+			log.Println("Invalid id", err)
+			res.Error = err.Error()
+			w.WriteHeader(400)
+			return
+		}
+
+		err = deletePerson(id)
+		if err != nil {
+			log.Println("Delete error", err)
+			res.Error = err.Error()
+			w.WriteHeader(400)
+			return
+		}
+
+		res.Message = "Person successfully deleted."
+	}
 }
 
 // Datalayer
